acbe301396f025c9c4449a7c7c073f17d5dfc7bc
Everaldo <everaldo2701@gmail.com>
2022-12-01 11:10:35 -0300

    Primeiro App com React

---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+import {  InputContainer} from './styles';
+
+const input = () => {
+    return (
+      <InputContainer>
+        <input disabled/>  
+      </InputContainer>
+    );
+  }
+  
+  export default input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+width: 100%;
+height: 75px;
+background: #acacff;
+
+display: flex;
+align-items: center;
+justify-content: flex-end;
+font-size: 24px;
+font-family: 'Roboto';
+
+input {
+    width: 100%;
+    height: 75px;
+    background: #acff;
+    border: 0;
+    font-size: 30px;
+    font-family: 'Roboto';
+    padding: 0 10px;
+
+    display: flex;
+    align-items: flex-end;
+    flex-direction: column;
+
+}
+`
